4295cc9af15530905e392d6eebbc3efed136256a
Yalin <523031393@qq.com>
2016-06-24 14:12:58 +0800

    Alert use uialertcontroller alert

--- Vender/Alert.swift
+++ Vender/Alert.swift
@@ -10,34 +10,49 @@ import UIKit
 
 struct Alert {
     static func showErrorAlert(title: String?, message: String?) {
-        UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: "确定").show()
+        Alert.showAlert(title, message: message)
     }
     
     static func showError(error: NSError) {
-        UIAlertView(title: error.domain, message: error.localizedDescription, delegate: nil, cancelButtonTitle: "取消").show()
+        Alert.showAlert(error.domain, message: error.localizedDescription)
     }
     
     static func showAlert(title: String?, message: String?) {
-        
+        Alert.showAlert(title, message: message, complete: nil)
     }
     
-    static func showAlert(title: String?, message: String?, complete: (Int)?) {
-        
+    static func showAlert(title: String?, message: String?, complete: ((Int) -> Void)?) {
+        Alert.showAlert(title, message: message, cancelButtonTitle: "取消", complete: complete, controller: nil, otherButtonTitles: nil)
     }
     
-    static func showAlert(title: String?, message: String?, cancelButtonTitle: String?, complete: (alert: Alert, buttonIndex: Int)?, controller: UIViewController?, otherButtonTitles: String?...) {
+    static func showAlert(title: String?, message: String?, cancelButtonTitle: String?, complete: ((Int) -> Void)?, controller: UIViewController?, otherButtonTitles: String?...) {
         
-    }
-    
-    static func showAlert(title: String?, message: String?, style: UIAlertViewStyle?, cancelButtonTitle: String?, complete: (alert: Alert, buttonIndex: Int)?, controller: UIViewController?, otherButtonTitles: String?...) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         
-    }
-    
-    static func showAlert(title: String?, message: String?, style: UIAlertViewStyle?, textFieldDefaultTexts: [String]?, cancelButtonTitle: String?, complete: (alert: Alert, buttonIndex: Int)?, controller: UIViewController?, otherButtonTitles: String?...) {
+        if cancelButtonTitle != nil {
+            let cancelAction = UIAlertAction(title: cancelButtonTitle, style: UIAlertActionStyle.Cancel, handler: { (action: UIAlertAction) in
+                complete?(0)
+            })
+            alertController.addAction(cancelAction)
+        }
         
+        for i in 0 ..< otherButtonTitles.count {
+            let action = UIAlertAction(title: otherButtonTitles[i], style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction) in
+                complete?(i + 1)
+            })
+            alertController.addAction(action)
+        }
+        
+        if controller != nil {
+            controller?.presentViewController(alertController, animated: true, completion: nil)
+        }
+        else {
+            (UIApplication.sharedApplication().delegate as? AppDelegate)?.window?.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+        }
     }
+}
+
+
+extension Alert {
     
-    func textField(index: UInt) -> UITextField? {
-        return nil
-    }
 }
